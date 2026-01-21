04388a5a6b1a32e942aff641a40386329d8544bf
jeonginh0 <stitch010913@gmail.com>
2026-01-21 16:27:12 +0900

    study(26.1.21): 6-1 ~ 6-4(6 end)학습 완료

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -55,8 +55,8 @@ public class QuerydslBasicTest {
 
         Member member1 = new Member("member1", 10, teamA);
         Member member2 = new Member("member2", 20, teamA);
-        Member member3 = new Member("member3", 10, teamB);
-        Member member4 = new Member("member4", 20, teamB);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
         em.persist(member1);
         em.persist(member2);
         em.persist(member3);
